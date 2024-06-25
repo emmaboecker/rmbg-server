ec3a6840cb9ec330f1ac1b5be7816aec581302de
Emma Böcker <stckoverflw@gmail.com>
2024-06-25 15:46:03 +0200

    use fenix for rust toolchain

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   description = "rmbg-server";
 
   inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
@@ -9,7 +13,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages.default = pkgs.callPackage ./nix/package.nix {};
+        packages.default = pkgs.callPackage ./nix/package.nix { fenix = inputs.fenix; nixpkgs = inputs.nixpkgs; };
       };
       flake = {};
     };
--- nix/package.nix
+++ nix/package.nix
@@ -1,11 +1,18 @@
-{ rustPlatform, lib, config, ... }:
-  rustPlatform.buildRustPackage {
+{ lib, fenix, nixpkgs, system, ... }:
+let
+  toolchain = fenix.packages.${system}.minimal.toolchain;
+  pkgs = nixpkgs.legacyPackages.${system};
+in
+  (pkgs.makeRustPlatform {
+    cargo = toolchain;
+    rustc = toolchain;
+  }).buildRustPackage {
     pname = "rmbg-server";
     version = (builtins.fromTOML (builtins.readFile ../Cargo.toml)).package.version;
     src = ../.;
     cargoLock.lockFile = ../Cargo.lock;
     meta = with lib; {
-      description = config.description;
+      description = "Remove Background Server";
       homepage = "https://github.com/emmaboecker/rmbg-server";
       license = licenses.agpl3Only;
     };
